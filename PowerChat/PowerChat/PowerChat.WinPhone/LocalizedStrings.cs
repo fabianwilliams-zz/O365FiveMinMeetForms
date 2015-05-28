211cf60989530f64b24e334c3bd85567fa822ee3
Fabian Williams <fabian@adotob.com>
2015-05-27 23:21:19 -0500

    Created a Xamarin.Forms Project from Visual Studio

---
+++ PowerChat/PowerChat/PowerChat.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using PowerChat.WinPhone.Resources;
+
+namespace PowerChat.WinPhone
+{
+	/// <summary>
+	/// Provides access to string resources.
+	/// </summary>
+	public class LocalizedStrings
+	{
+		private static AppResources _localizedResources = new AppResources();
+
+		public AppResources LocalizedResources { get { return _localizedResources; } }
+	}
+}
